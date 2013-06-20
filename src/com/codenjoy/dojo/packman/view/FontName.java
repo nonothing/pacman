a4e9966b9425c0ac7a236bebcdc3f68c3fa4a1a6
nonothing <all.nothing.name@gmail.com>
2013-06-20 20:26:11 +0300

    почистил

--- src/com/codenjoy/dojo/packman/view/FontName.java
+++
@@ -1,5 +0,0 @@
-package com.codenjoy.dojo.packman.view;
-
-public enum FontName {
-    emulogic, pacfont
-}
--- src/com/codenjoy/dojo/packman/view/Wave.java
+++
@@ -1,6 +0,0 @@
-package com.codenjoy.dojo.packman.view;
-
-public enum Wave {
-    beginning, eatFruit, eatSpirit,sirenSound,chomp,death,
-     menuSound
-}
